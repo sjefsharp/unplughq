ffbd618d053489422b47627f7635dca64bd92c6f
Sjefsharp <sjef.jenniskens@me.com>
2026-03-16 09:17:13 +0100

    fix(fe): add output standalone to next.config.ts AB#273

--- code/next.config.ts
+++ code/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   serverExternalPackages: ["argon2", "ssh2", "bullmq", "pino"],
   async headers() {
     return [{
